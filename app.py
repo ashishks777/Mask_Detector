2713df9450a96aa99f3dd6eae97dccd2319a9b51
ashishks777 <88076346+ashishks777@users.noreply.github.com>
2021-07-30 09:14:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-pip install tensorflow==2.5.0rc0
+
 from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
 from tensorflow.keras.preprocessing.image import img_to_array
 from tensorflow.keras.models import load_model
