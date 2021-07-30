d2549979835eb0170f15b573679804f479f69b4f
ashishks777 <88076346+ashishks777@users.noreply.github.com>
2021-07-30 10:04:02 +0530

    Update camera.py

--- camera.py
+++ camera.py
@@ -3,7 +3,7 @@ from tensorflow.keras.preprocessing.image import img_to_array
 from tensorflow.keras.models import load_model
 
 import numpy as np
-import cv2
+
 import os
 from imutils.video import VideoStream 
 import imutils
